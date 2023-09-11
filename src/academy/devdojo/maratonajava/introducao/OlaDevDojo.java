130818dbbdda5674ff1bd808cea4b5cbdf3c5020
João-Carlos <joaocarlosdosreia@gmail.com>
2023-09-11 14:08:58 -0300

    Estudos Java curso dev dojo dia 06/09

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+// Isso é um comentário de uma linha
+
+/* -----------------------------------------
+Isto é um comentário de várias linhas
+-------------------------------------------- */
+
+/**
+ * Isso é um comentário javadock
+ */
+public class OlaDevDojo {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
