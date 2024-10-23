af6eb901796947e0bc61dd9a93758362b171c4ae
João-Carlos <joaocarlosdosreia@gmail.com>
2024-10-23 15:06:47 -0300

    08/07/2024

---
+++ src/academy/resume/Aula03Exercicio.java
@@ -0,0 +1,22 @@
+package academy.resume;
+
+import java.util.Scanner;
+
+public class Aula03Exercicio {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double salary;
+        String name, adress, date;
+
+        System.out.println("Diga seu nome");
+        name = sc.nextLine();
+        System.out.println("Diga seu endereço");
+        adress = sc.nextLine();
+        System.out.println("Diga a data de hoje");
+        date = sc.nextLine();
+        System.out.println("Diga seu endereço");
+        salary = sc.nextDouble();
+        System.out.println("Eu " + name + ", morando no endereço " + adress + ", confirmo que recebi o sálario de " + salary + " na data " + date);
+    }
+
+}
